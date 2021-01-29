19f86d71f552fe6e053e22c5c62f6c1456de9160
Krotkov <krotkovd99@gmail.com>
2021-01-29 07:19:48 +0300

    small fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -26,7 +26,7 @@ router.get('/login', (req, res) => {
     }
 })
 
-router.get('/register', (req, res) => {
+router.post('/register', (req, res) => {
     if (req.query.login in userController.users) {
         res.status(400).send();
     } else {
